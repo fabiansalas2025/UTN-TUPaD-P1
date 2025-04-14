41820e0df065b319d3442cf2bd8ab4155a32ba25
Fabián <fabio.salas.fs@gmail.com>
2025-04-13 23:20:46 -0300

    Add files via upload

---
+++ 03 Estructuras Condicionales/SALAS_TP-03.py
@@ -0,0 +1,50 @@
+
+# ACTIVIDAD 1
+
+edad = int(input("Ingrese su edad, por favor: "))
+if edad > 18:
+    print("Es mayor de edad.")
+
+# ACTIVIDAD 2
+
+nota = float(input("Ingrese su nota: "))
+if nota >= 6:
+    print("Aprobado.")
+else:
+    print("Desaprobado.")
+
+# ACTIVIDAD 3
+
+nro = int(input("Ingrese un número entero :"))
+if nro % 2 == 0:
+    print(f"{nro} es par.")
+else:
+    print(f"{nro} es impar.")
+
+# ACTIVIDAD 4
+
+edad = int(input("Ingrese su edad :"))
+if (edad >= 12) and (edad < 18):
+    print("Adolescente.")
+elif (edad >= 18) and (edad < 30):
+    print("Adulto/a joven.")
+elif (edad >= 30):
+    print("Adulto/a.")
+else:
+    print("Niño/a")
+
+# ACTIVIDAD 5
+
+psw = len(input("Ingrese contraseña : "))
+if (psw >= 8) and (psw <= 12):
+    print("Ha ingresado una contraseña correcta.")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres.")
+
+# ACTIVIDAD 6
+
+# ACTIVIDAD 7
+
+
+
+
